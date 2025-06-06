8b2168697c463927ae3ce2582855c7c638f5f365
bschm0622 <rfcooper95@gmail.com>
2025-06-05 22:56:40 -0400

    auto-save

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -4,3 +4,31 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+/**
+ * Creates a debounced version of a function that delays its execution until after
+ * the specified wait time has elapsed since the last time it was called.
+ * 
+ * @param func The function to debounce
+ * @param wait The number of milliseconds to delay
+ * @returns A debounced version of the input function
+ */
+export const debounce = <T extends (...args: any[]) => any>(
+  func: T,
+  wait: number
+): ((...args: Parameters<T>) => void) & { cancel: () => void } => {
+  let timeout: NodeJS.Timeout;
+
+  const debounced = ((...args: Parameters<T>) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), wait);
+  }) as T & { cancel: () => void };
+
+  debounced.cancel = () => {
+    clearTimeout(timeout);
+  };
+
+  return debounced;
+};
+
+export const AUTO_SAVE_DELAY = 1000; // 1 second
